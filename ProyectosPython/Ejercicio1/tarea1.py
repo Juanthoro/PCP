5f799fdef28c31a8c87f31f19c7591cf6bb81f34
Juanthoro <alumno.1544094@ies-azarquiel.es>
2025-10-15 15:54:11 +0200

    Formato

--- ProyectosPython/Ejercicio1/tarea1.py
+++ ProyectosPython/Ejercicio1/tarea1.py
@@ -1,4 +1,5 @@
 """
+###########################################################################################################
 # Dados los catetos dar el valor de la hipotenusa
 import math
 
@@ -6,10 +7,11 @@ catetoa = float(input("Dame el valor del cateto a"))
 catetob = float(input("Dame el valor del cateto b"))
 hipotenusa = math.pow(catetoa,2) + math.pow(catetob,2)
 print(math.sqrt(hipotenusa))
-
+###########################################################################################################
 """
 
 """
+###########################################################################################################
 # Bucle
 # La condición de salida debe ponerse lo ultimo para que entre el valor que definimos primero
 # si no no entraria.
@@ -23,8 +25,10 @@ while contador < 20 :
         print(f"Llegamos al 15")
         break
     print(f"El valor de contador es {contador}")
+###########################################################################################################
 """
 
+###########################################################################################################
 # Tabla de multiplicar
 # Aqui el error seria no castear a int en la entrada del valor, lo que provocaria que pusiera el valor
 # de la variable las veces que hemos puesto=> 3, 33, 333, 3333, etc.
@@ -33,5 +37,6 @@ variabletabla = int(input("Dime la tabla de multiplicar que quieres :"))
 for i in range (11):
     resultado = variabletabla*i
     print(f"{variabletabla} x {i} = {resultado}")
+###########################################################################################################
 
 
