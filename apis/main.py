0b846aee5ea4c9626a717023d62b9eb6a97ecffd
Juanthoro <alumno.1544094@ies-azarquiel.es>
2025-11-25 18:02:56 +0100

    seguimos api

--- apis/main.py
+++ apis/main.py
@@ -4,12 +4,17 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/my-first-api")
-def hello():
-    return ("Hello World!")
+# @app.get("/my-first-api")
+# def hello():
+#     return ("Hello World!")
 
 # Ahora nos vamos al navegador y ponemos en la barra "http://localhost:8000/my-first-api"
 # y debería salirme Hello World!
 
+@app.get("/my-first-api")
+def hello(name: str):
+  return {'Hello ' + name + '!'}
 
+# # Ahora nos vamos al navegador y ponemos en la barra "http://localhost:8000/my-first-api?name=Juan"
+# y nos saldria Hello Juan!
 
